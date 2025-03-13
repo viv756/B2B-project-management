89f7feb412740a49b91ed9972cc35c4afbf690b8
*vivek <vrvivekofficial@gmail.com>
2025-03-13 15:06:51 +0530

    create server and test

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -12,6 +12,9 @@ const appConfig = () => ({
   GOOGLE_CLIENT_ID: getEnv("GOOGLE_CLIENT_ID"),
   GOOGLE_CLIENT_SECRET: getEnv("GOOGLE_CLIENT_SECRET"),
   GOOGLE_CALLBACK_URL: getEnv("GOOGLE_CALLBACK_URL"),
+
+  FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "localhost"),
+  FRONTEND_GOOGLE_CALLBACK_URL: getEnv("FRONTEND_GOOGLE_CALLBACK_URL"),
 })
 
 export const config = appConfig()
---
+++ backend/src/index.ts
@@ -0,0 +1,39 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get(`/`, (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    message: "Hello",
+  });
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+});
