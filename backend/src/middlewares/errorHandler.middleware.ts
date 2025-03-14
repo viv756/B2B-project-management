a43764320a246abd34ca1df24aa614cbad109f90
*vivek <vrvivekofficial@gmail.com>
2025-03-14 10:42:35 +0530

    create apperror middleware and create exceptions

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler } from "express";
 import { HTTPSTATUS } from "../config/http.config";
+import { AppError } from "../utils/appError";
 
 export const errorHandler: ErrorRequestHandler = (error, req, res, next): any => {
   console.error(`Error occured on PATH ${req.path}`, error);
@@ -7,6 +8,13 @@ export const errorHandler: ErrorRequestHandler = (error, req, res, next): any =>
   if (error instanceof SyntaxError) {
     return res.status(HTTPSTATUS.BAD_REQUEST).json({
       message: "Invalid JSON format. Please check your request",
+    });
+  }
+
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+      errorCode: error.errorCode,
     });
   }
 
