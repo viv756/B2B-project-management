e50e887bfe113aa3465ccad974a6fbf330d1b7df
*vivek <vrvivekofficial@gmail.com>
2025-03-17 15:19:08 +0530

    create role.Model

--- backend/src/models/members.model.ts
+++ backend/src/models/members.model.ts
@@ -1,9 +1,10 @@
 import mongoose, { Document, Schema } from "mongoose";
+import { RoleDocument } from "./roles-permission.model";
 
 export interface MemberDocument extends Document {
   userId: mongoose.Types.ObjectId;
   workspaceId: mongoose.Types.ObjectId;
-  // role: RoleDocument;
+  role: RoleDocument;
   joinedAt: Date;
 }
 
@@ -19,11 +20,11 @@ const memberSchema = new Schema<MemberDocument>(
       ref: "Workspace",
       required: true,
     },
-    // role: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "Role",
-    //   required: true,
-    // },
+    role: {
+      type: Schema.Types.ObjectId,
+      ref: "Role",
+      required: true,
+    },
     joinedAt: {
       type: Date,
       default: Date.now,
