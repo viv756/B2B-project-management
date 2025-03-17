d45687e753b528c80ea0b92bed161b544f5f7149
*vivek <vrvivekofficial@gmail.com>
2025-03-17 15:08:17 +0530

    create taskModel

--- backend/src/utils/uuid.ts
+++ backend/src/utils/uuid.ts
@@ -3,3 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 export function generateInviteCode() {
   return uuidv4().replace(/-/g, "").substring(0, 8);
 }
+
+export function generateTaskCode() {
+  return `task-${uuidv4().replace(/-/g, "").substring(0, 3)}`;
+}
