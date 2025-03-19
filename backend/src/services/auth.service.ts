04ee0783dabb9e13d157d2f2fffb1d2879efc533
*vivek <vrvivekofficial@gmail.com>
2025-03-19 14:58:23 +0530

    create passport google-oauth configuration and create user creation and login endpoint

---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+import UserModel from "../models/user.model";
+import AccountModel from "../models/account.model";
+import WorkspaceModel from "../models/workspace.model";
+import RoleModel from "../models/roles-permission.model";
+import { Roles } from "../enums/role.enum";
+import { NotFoundException } from "../utils/appError";
+import MemberModel from "../models/members.model";
+
+export const loginOrCreateAccountService = async (data: {
+  provider: string;
+  displayName: string;
+  providerId: string;
+  picture?: string;
+  email?: string;
+}) => {
+  const { providerId, provider, displayName, email, picture } = data;
+
+  const session = await mongoose.startSession();
+
+  try {
+    let user = await UserModel.findOne({ email }).session(session);
+    
+    if (!user) {
+      user = new UserModel({
+        email,
+        name: displayName,
+        profilePicture: picture || null,
+      });
+      await user.save({ session });
+
+      const account = new AccountModel({
+        userId: user._id,
+        provider: provider,
+        providerId: providerId,
+      });
+      await account.save({ session });
+
+      const workspace = new WorkspaceModel({
+        name: `My Workspace`,
+        description: `Workspace created for ${user.name}`,
+        owner: user._id,
+      });
+      await workspace.save({ session });
+
+      const ownerRole = await RoleModel.findOne({
+        name: Roles.OWNER,
+      }).session(session);
+
+      if (!ownerRole) {
+        throw new NotFoundException("Owner role not found");
+      }
+
+      const member = new MemberModel({
+        userId: user._id,
+        workspaceId: workspace._id,
+        role: ownerRole._id,
+        joinedAt: new Date(),
+      });
+      await member.save({ session });
+
+      user.currentWorkspace = workspace._id as mongoose.Types.ObjectId;
+      await user.save({ session });
+    }
+
+    await session.commitTransaction();
+    session.endSession();
+    console.log("End Session...");
+
+    return { user };
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
+  } finally {
+    session.endSession();
+  }
+};
