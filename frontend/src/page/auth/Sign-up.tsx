f4ea336f23211dc98b3fe3599e55b97bb3d945db
*vivek <vrvivekofficial@gmail.com>
2025-04-03 17:32:33 +0530

    create signup page

--- frontend/src/page/auth/Sign-up.tsx
+++ frontend/src/page/auth/Sign-up.tsx
@@ -1,7 +1,150 @@
-import React from "react";
+import GoogleOauthButton from "@/components/auth/google-oauth-button";
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { z } from "zod";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const formSchema = z.object({
+    name: z.string().trim().min(1, {
+      message: "Name is required",
+    }),
+    email: z.string().trim().email("Invalid email address").min(1, {
+      message: "Workspace name is required",
+    }),
+    password: z.string().trim().min(1, {
+      message: "Password is required",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link to="/" className="flex items-center gap-2 self-center font-medium">
+          <Logo />
+          Team Sync.
+        </Link>
+        <div className="flex flex-col gap-6">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Create an account</CardTitle>
+              <CardDescription>Signup with your Email or Google account</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <Form {...form}>
+                <form action="">
+                  <div className="grid gap-6">
+                    <div className="flex flex-col gap-4">
+                      <GoogleOauthButton label="Signup" />
+                    </div>
+                    <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                      <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                        Or continue with
+                      </span>
+                    </div>
+                    <div className="grid gap-2">
+                      <div className="grid gap-2">
+                        <FormField
+                          control={form.control}
+                          name="name"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="dark:text-[#f1f7feb5] text-sm">Name</FormLabel>
+                              <FormControl>
+                                <Input
+                                  placeholder="devin carloz"
+                                  className="!h-[48px]"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+
+                      <div className="grid gap-2">
+                        <FormField
+                          control={form.control}
+                          name="email"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="dark:text-[#f1f7feb5] text-sm">Email</FormLabel>
+                              <FormControl>
+                                <Input
+                                  placeholder="m@example.com"
+                                  className="!h-[48px]"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+
+                      <div className="grid gap-2">
+                        <FormField
+                          control={form.control}
+                          name="password"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                                Password
+                              </FormLabel>
+                              <FormControl>
+                                <Input type="password" className="!h-[48px]" {...field} />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+                      <Button
+                        type="submit"
+                        // disabled={isPending}
+                        className="w-full">
+                        {/* {isPending && <Loader className="animate-spin" />} */}
+                        Sign up
+                      </Button>
+                    </div>
+                    <div className="text-center text-sm">
+                      Already have an account?{" "}
+                      <Link to="/" className="underline underline-offset-4">
+                        Sign in
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </Form>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
