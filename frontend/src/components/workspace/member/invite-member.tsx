650e6f104dcf8e6ea7c4bb23de399c48aded9943
*vivek <vrvivekofficial@gmail.com>
2025-04-23 14:41:05 +0530

    create invite user component

---
+++ frontend/src/components/workspace/member/invite-member.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import PermissionsGuard from "@/components/reusable/permission-guard";
+import { Permissions } from "@/constant";
+import { useAuthContext } from "@/context/auth.provider";
+import { CheckIcon, CopyIcon, Loader } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { BASE_ROUTE } from "@/routes/common/routePaths";
+import { toast } from "sonner";
+
+const InviteMember = () => {
+  const { workspace, workspaceLoading } = useAuthContext();
+  const [copied, setCopied] = useState(false);
+
+  const inviteUrl = workspace
+    ? `${window.location.origin}${BASE_ROUTE.INVITE_URL.replace(
+        ":inviteCode",
+        workspace.inviteCode
+      )}`
+    : "";
+
+  const handleCopy = () => {
+    if (inviteUrl) {
+      navigator.clipboard.writeText(inviteUrl).then(() => {
+        setCopied(true);
+        toast.success("Invite url copied to clipboard", {
+          description: Date.now(),
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        });
+        setTimeout(() => setCopied(false), 2000);
+      });
+    }
+  };
+
+  return (
+    <div className="flex flex-col pt-0.5 px-0 ">
+      <h5 className="text-lg  leading-[30px] font-semibold mb-1">Invite members to join you</h5>
+      <p className="text-sm text-muted-foreground leading-tight">
+        Anyone with an invite link can join this free Workspace. You can also disable and create a
+        new invite link for this Workspace at any time.
+      </p>
+
+      <PermissionsGuard showMessage requiredPermission={Permissions.ADD_MEMBER}>
+        {workspaceLoading ? (
+          <Loader className="w-8 h-8 animate-spin place-self-center flex" />
+        ) : (
+          <div className="flex py-3 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Link
+            </Label>
+            <Input
+              id="link"
+              disabled={true}
+              className="disabled:opacity-100 disabled:pointer-events-none"
+              value={inviteUrl}
+              readOnly
+            />
+            <Button disabled={false} className="shrink-0" size="icon" onClick={handleCopy}>
+              {copied ? <CheckIcon /> : <CopyIcon />}
+            </Button>
+          </div>
+        )}
+      </PermissionsGuard>
+    </div>
+  );
+};
+
+export default InviteMember;
--- frontend/src/page/workspace/Members.tsx
+++ frontend/src/page/workspace/Members.tsx
@@ -1,9 +1,26 @@
 import WorkspaceHeader from "@/components/common/workspace-header";
+import { Separator } from "@/components/ui/separator";
+import InviteMember from "@/components/workspace/member/invite-member";
 
 const Members = () => {
   return (
     <div className="w-full h-auto pt-2">
       <WorkspaceHeader />
+      <Separator className="my-4" />
+      <main>
+        <div className="w-full max-w-3xl mx-auto pt-3">
+          <div>
+            <h2 className="text-lg leading-[30px] font-semibold mb-1">Workspace members</h2>
+            <p className="text-sm text-muted-foreground">
+              Workspace members can view and join all Workspace project, tasks and create new task
+              in the Workspace.
+            </p>
+          </div>
+          <Separator className="my-4" />
+          {/* Invite user  */}
+          <InviteMember />
+        </div>
+      </main>
     </div>
   );
 };
