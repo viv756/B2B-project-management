e5b22980b3e81767a4e30408f8fdd21a247876d3
*vivek <vrvivekofficial@gmail.com>
2025-04-25 14:31:20 +0530

    fix: resolve <a> tag nesting warning in SignIn and SignUp components
    - Removed invalid nested anchor tags to clean up console warnings
    - Ensures semantic HTML and avoids accessibility issues

--- frontend/src/page/auth/Sign-in.tsx
+++ frontend/src/page/auth/Sign-in.tsx
@@ -70,10 +70,10 @@ const SignIn = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link to="/" className="flex items-center gap-2 self-center font-medium">
+        <div className="flex items-center gap-2 self-center font-medium">
           <Logo />
-          Team Sync
-        </Link>
+          Team Sync.
+        </div>
         <div className="flex flex-col gap-6">
           <Card>
             <CardHeader className="text-center">
--- frontend/src/page/auth/Sign-up.tsx
+++ frontend/src/page/auth/Sign-up.tsx
@@ -69,13 +69,13 @@ const SignUp = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link to="/" className="flex items-center gap-2 self-center font-medium">
+        <div className="flex items-center gap-2 self-center font-medium">
           <Logo />
           Team Sync.
-        </Link>
+        </div>
         <div className="flex flex-col gap-6">
           <Card>
-            <CardHeader>
+            <CardHeader className="text-center">
               <CardTitle className="text-xl">Create an account</CardTitle>
               <CardDescription>Signup with your Email or Google account</CardDescription>
             </CardHeader>
