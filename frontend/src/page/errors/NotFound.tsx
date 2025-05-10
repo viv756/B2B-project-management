8f11e6eb35d7bc44ea53c325b036188d27364f67
*vivek <vrvivekofficial@gmail.com>
2025-05-10 12:15:32 +0530

    feat: create notfound ui

--- frontend/src/page/errors/NotFound.tsx
+++ frontend/src/page/errors/NotFound.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+      <div className="w-full space-y-6 text-center">
+        <div className="space-y-3">
+          <h1 className="text-4xl tracking-tighter sm:text-5xl">404 Page Not Found</h1>
+          <p className="text-gray-500">
+            Sorry, we couldn&#x27;t find the page you&#x27;re looking for.
+          </p>
+        </div>
+        <Button
+          onClick={() => {
+            navigate(-1);
+          }}>
+          Return to website
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
