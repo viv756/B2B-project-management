61f5b8f28f972ed49939e01f91fab79da1ac4b28
*vivek <vrvivekofficial@gmail.com>
2025-05-20 09:51:40 +0530

    changed vit.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true, // optional fix for file watching inside containers
+    },
   },
   plugins: [react(), tailwindcss()],
   resolve: {
